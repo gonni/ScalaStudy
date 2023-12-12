0fd55496985a97eea877a1a3798876ee041d9fc8
ygkim <gonni21c@gmail.com>
2023-12-13 07:53:31 +0900

    daily

---
+++ src/main/scala/c/x/sample/mid/ExtraLongFactorials.scala
@@ -0,0 +1,20 @@
+package c.x.sample.mid
+
+import scala.annotation.tailrec
+
+object ExtraLongFactorials extends App {
+  def extraLongFactorials(n: Int) = {
+    // Write your code here
+    @tailrec
+    def multi(n: Int, mulVal: BigInt = 1): BigInt = {
+      if(n < 1) return mulVal
+      val rVal: BigInt = mulVal * n
+      multi(n - 1, rVal)
+    }
+
+    print(multi(n))
+  }
+
+  extraLongFactorials(30)
+//  println("->" + extraLongFactorials(30))
+}
